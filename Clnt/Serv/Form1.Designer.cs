1d1a388f4dff1199a29db17b5d796ff93fd6f4cd
HDUZZB <49432961+HDUZZB@users.noreply.github.com>
2020-02-06 18:40:45 +0800

    客户端文件

---
+++ Clnt/Serv/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Serv
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labport = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtSelectFile = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.DiagSelectFile = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.DownCmd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(561, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP  :";
+            // 
+            // labport
+            // 
+            this.labport.AutoSize = true;
+            this.labport.Location = new System.Drawing.Point(561, 102);
+            this.labport.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labport.Name = "labport";
+            this.labport.Size = new System.Drawing.Size(47, 15);
+            this.labport.TabIndex = 1;
+            this.labport.Text = "PORT:";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(632, 58);
+            this.txtIp.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(132, 25);
+            this.txtIp.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(632, 102);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(132, 25);
+            this.txtPort.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(632, 240);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 29);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "连接服务器";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnBeginListen_Click_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(632, 312);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 29);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "接收消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click_1);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(587, 489);
+            this.btnSendFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(100, 29);
+            this.btnSendFile.TabIndex = 9;
+            this.btnSendFile.Text = "发送文件";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click_1);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(456, 491);
+            this.btnSelectFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(100, 29);
+            this.btnSelectFile.TabIndex = 10;
+            this.btnSelectFile.Text = "选择文件";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtSelectFile
+            // 
+            this.txtSelectFile.Location = new System.Drawing.Point(32, 491);
+            this.txtSelectFile.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSelectFile.Name = "txtSelectFile";
+            this.txtSelectFile.Size = new System.Drawing.Size(393, 25);
+            this.txtSelectFile.TabIndex = 11;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(17, 25);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(489, 236);
+            this.txtMsg.TabIndex = 12;
+            // 
+            // DiagSelectFile
+            // 
+            this.DiagSelectFile.FileName = "diagSelectFile";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(561, 156);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "昵称：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(632, 156);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 25);
+            this.txtName.TabIndex = 3;
+            // 
+            // DownCmd
+            // 
+            this.DownCmd.Location = new System.Drawing.Point(632, 276);
+            this.DownCmd.Margin = new System.Windows.Forms.Padding(4);
+            this.DownCmd.Name = "DownCmd";
+            this.DownCmd.Size = new System.Drawing.Size(100, 29);
+            this.DownCmd.TabIndex = 13;
+            this.DownCmd.Text = "下载程序";
+            this.DownCmd.UseVisualStyleBackColor = true;
+            this.DownCmd.Click += new System.EventHandler(this.DownCmd_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(32, 312);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(412, 94);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "LED流水灯显示";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(308, 26);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 41);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "LED4";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(216, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 41);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "LED3";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(124, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 41);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "LED2";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(20, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 41);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "LED1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMsg);
+            this.groupBox2.Location = new System.Drawing.Point(15, 31);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(517, 272);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "信息显示";
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 536);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DownCmd);
+            this.Controls.Add(this.txtSelectFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.labport);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmClient";
+            this.Text = "客户端";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labport;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtSelectFile;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.OpenFileDialog DiagSelectFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button DownCmd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Clnt/Serv/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;  // IP，IPAddress, IPEndPoint，端口等；
+using System.Threading;
+using System.IO;
+
+namespace Serv
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread threadClient = null; // 创建用于接收服务端消息的 线程；
+        Socket sockClient = null;
+
+
+        private void btnBeginListen_Click_1(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse(txtIp.Text.Trim());
+            IPEndPoint endPoint = new IPEndPoint(ip, int.Parse(txtPort.Text.Trim()));
+            sockClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                ShowMsg("与服务器连接中……");
+                sockClient.Connect(endPoint);
+
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message);
+                return;
+                //this.Close();
+            }
+            ShowMsg("与服务器连接成功！！！");
+            threadClient = new Thread(RecMsg);
+            threadClient.IsBackground = true;
+            threadClient.Start();
+
+        }
+
+        void RecMsg()
+        {
+            while (true)
+            {
+                // 定义一个2M的缓存区；
+                byte[] arrMsgRec = new byte[1024 * 1024 * 2];
+                // 将接受到的数据存入到输入  arrMsgRec中；
+                int length = -1;
+                try
+                {
+                    length = sockClient.Receive(arrMsgRec); // 接收数据，并返回数据的长度；
+                }
+                catch (SocketException se)
+                {
+                    ShowMsg("异常；" + se.Message);
+                    return;
+                }
+                catch (Exception e)
+                {
+                    ShowMsg("异常：" + e.Message);
+                    return;
+                }
+                if (arrMsgRec[0] == 0) // 表示接收到的是消息数据；
+                {
+                    string strMsg = System.Text.Encoding.UTF8.GetString(arrMsgRec, 1, length - 1);// 将接受到的字节数据转化成字符串；
+                    string str_tmp = strMsg;
+                   // string str_tmp = strMsg.Substring(3);
+                    label3.BackColor = System.Drawing.Color.Gray;
+                    label4.BackColor = System.Drawing.Color.Gray;
+                    label5.BackColor = System.Drawing.Color.Gray;
+                    label6.BackColor = System.Drawing.Color.Gray;
+                    if (str_tmp == "FE ")
+                    {
+
+                        label3.BackColor = System.Drawing.Color.Red;
+                    }
+                    else if (str_tmp == "FD ")
+                    {
+
+                        label4.BackColor = System.Drawing.Color.Red;
+
+                    }
+                    else if (str_tmp == "FB ")
+                    {
+
+                        label5.BackColor = System.Drawing.Color.Red;
+                    }
+                    else if (str_tmp == "F7 ")
+                    {
+
+                        label6.BackColor = System.Drawing.Color.Red;
+                    }
+
+                    //ShowMsg(str_tmp);
+                }
+                if (arrMsgRec[0] == 1) // 表示接收到的是文件数据；
+                {
+
+                    try
+                    {
+                        SaveFileDialog sfd = new SaveFileDialog();
+
+                        if (sfd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                        {// 在上边的 sfd.ShowDialog（） 的括号里边一定要加上 this 否则就不会弹出 另存为 的对话框，而弹出的是本类的其他窗口，，这个一定要注意！！！【解释：加了this的sfd.ShowDialog(this)，“另存为”窗口的指针才能被SaveFileDialog的对象调用，若不加thisSaveFileDialog 的对象调用的是本类的其他窗口了，当然不弹出“另存为”窗口。】
+
+                            string fileSavePath = sfd.FileName;// 获得文件保存的路径；
+                            // 创建文件流，然后根据路径创建文件；
+                            using (FileStream fs = new FileStream(fileSavePath, FileMode.Create))
+                            {
+                                fs.Write(arrMsgRec, 1, length - 1);
+                                ShowMsg("文件保存成功：" + fileSavePath);
+                            }
+                        }
+                    }
+                    catch (Exception aaa)
+                    {
+                        MessageBox.Show(aaa.Message);
+                    }
+                }
+            }
+        }
+        void ShowMsg(string str)
+        {
+            txtMsg.AppendText(str + "\r\n");
+        }
+
+        // 发送消息；
+        private void btnSend_Click_1(object sender, EventArgs e)
+        {
+            string strMsg = txtName.Text.Trim() + "\r\n" + "开始接收来自服务器的数据" +  "\r\n";
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(strMsg);
+            byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+            arrSendMsg[0] = 0; // 用来表示发送的是消息数据
+            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+            sockClient.Send(arrSendMsg); // 发送消息；
+            ShowMsg(strMsg);
+            
+        }
+
+        // 选择要发送的文件；
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "D:\\";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSelectFile.Text = ofd.FileName;
+            }
+        }
+
+
+        // 文件的发送
+        private void btnSendFile_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSelectFile.Text))
+            {
+                MessageBox.Show("请选择要发送的文件！！！");
+            }
+            else
+            {
+                // 用文件流打开用户要发送的文件；
+                using (FileStream fs = new FileStream(txtSelectFile.Text, FileMode.Open))
+                {
+                    //在发送文件以前先给好友发送这个文件的名字+扩展名，方便后面的保存操作；
+                    string fileName = System.IO.Path.GetFileName(txtSelectFile.Text);
+                    string fileExtension = System.IO.Path.GetExtension(txtSelectFile.Text);
+                    string strMsg = "我给你发送的文件为： " + fileName + "\r\n";
+                    byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(strMsg);
+                    byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+                    arrSendMsg[0] = 0; // 用来表示发送的是消息数据
+                    Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+                    sockClient.Send(arrSendMsg); // 发送消息；
+
+                    byte[] arrFile = new byte[1024 * 1024 * 2];
+                    int length = fs.Read(arrFile, 0, arrFile.Length);  // 将文件中的数据读到arrFile数组中；
+                    byte[] arrFileSend = new byte[length + 1];
+                    arrFileSend[0] = 1; // 用来表示发送的是文件数据；
+                    Buffer.BlockCopy(arrFile, 0, arrFileSend, 1, length);
+                    // 还有一个 CopyTo的方法，但是在这里不适合； 当然还可以用for循环自己转化；
+                    sockClient.Send(arrFileSend);// 发送数据到服务端；
+                    txtSelectFile.Clear();
+                }
+            }
+        }
+
+        private void DownCmd_Click(object sender, EventArgs e)
+        {
+            string strMsg = "下载程序";
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(strMsg);
+            byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+            arrSendMsg[0] = 2; // 用来表示发送的是消息数据
+            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+            sockClient.Send(arrSendMsg); // 发送消息；
+            ShowMsg(strMsg);
+            
+        }
+
+
+    }
+
+
+}
+
+    
